90cc31e802445c511879061cc782bc8d9e38b806
Elliot Williams <elliotdwilliams@gmail.com>
2023-01-27 15:37:20 -0600

    Initial commit

---
+++ dpla_collection_by_id.py
@@ -0,0 +1,44 @@
+# This script takes a list of DPLA identifiers, then queries the DPLA API to get the collection name for that item.
+# Developed to take a list of items from the DPLA Analytics Dashboard (such as catalog views or click-throughs), and analyze them by collection.
+# Accepts a filename as a command line argument; that file should include a list of DPLA identifiers
+
+from dpla.api import DPLA
+from time import sleep
+import sys
+
+#get filename from command line argument
+input_filename = sys.argv[1]
+
+#create DPLA object using dpla module and your API key
+dpla = DPLA('9e772db07b96bf5971582a9e95d873ef')
+
+#Opens file with identifiers and then reads them into a list
+file_identifiers = open(input_filename,"r")
+identifiers = file_identifiers.readlines()
+
+#print(identifiers)
+
+#open file for writing results
+file_results = open(r"SearchResults.txt","w")
+file_results.write("IDENTIFIER"+"\t"+"INSTITUTION"+"\t"+"COLLECTION"+"\n")
+
+for identifier in identifiers:
+    identifier = identifier.rstrip()
+
+    try:
+        result = dpla.fetch_by_id([identifier])
+
+        institution = result.items[0]["dataProvider"]["name"]
+        collection = result.items[0]["sourceResource"]["collection"][0]["title"]
+
+        print(identifier+"\t"+institution+"\t"+collection)
+        file_results.write(identifier+"\t"+institution+"\t"+collection+"\n")
+
+    #error handling for when DPLA API doesn't return the item record (i.e. the item is no longer in DPLA)
+    except AttributeError:
+        print(identifier+"\t"+"record not found"+"\t"+"record not found")
+        file_results.write(identifier+"\t"+"record not found"+"\t"+"record not found"+"\n")
+
+    sleep(0.50)
+
+file_results.close()
